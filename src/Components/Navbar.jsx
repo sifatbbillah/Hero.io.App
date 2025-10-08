7ac643584bb48a138687177febaca5675674eab3
sifatbbillah <sifatbillah82@gmail.com>
2025-10-08 22:44:47 +0600

    change

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,21 +2,22 @@ import { FaGithub } from "react-icons/fa";
 import { NavLink } from "react-router";
 import logo from "../assets/logo.png";
 import Container from "./Container";
+
 const Navbar = () => {
   return (
     <div className="bg-white py-2">
       <Container>
-        <div className="navbar grid grid-cols-3 justify-center items-center ">
+        <div className="navbar grid grid-cols-3 justify-center items-center">
           <div className="col-span-2 md:col-span-1 flex justify-start">
             <NavLink
               to={"/"}
-              className="flex gap-2 justify-center items-center text-2xl uppercase font-bold  
-           gradient-text"
+              className="flex gap-2 justify-center items-center text-2xl uppercase font-bold gradient-text"
             >
               <img className="w-10" src={logo} alt="" /> Hero.io
             </NavLink>
           </div>
-          <div className="col-span-3 mt-5 md:mt-0 justify-center order-3 flex gap-5 text-[16px] font-medium text-black md:order-2 md:col-span-1 ">
+
+          <div className="col-span-3 mt-5 md:mt-0 justify-center order-3 flex gap-5 text-[16px] font-medium text-black md:order-2 md:col-span-1">
             <NavLink
               to={"/"}
               className={({ isActive }) =>
@@ -42,10 +43,11 @@ const Navbar = () => {
               Installation
             </NavLink>
           </div>
-          <div className=" md:order-3 md:col-span-1 col-span-1 flex justify-end">
+
+          <div className="md:order-3 md:col-span-1 col-span-1 flex justify-end">
             <a
-             
-              target="blank"
+              href="https://github.com/sifatbbillah"
+              target="_blank"
               className="btn gradient-btn"
             >
               <FaGithub /> Contribute
